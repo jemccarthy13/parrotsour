b778b3cac80e6519845a0d2e5814677f5755f5cf
John McCarthy <jemccarthy13@gmail.com>
2023-01-17 15:37:04 +0200

    chore: move version notification into component

---
+++ src/components/home/version-notification.tsx
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect } from "react"
+import { Cookies } from "react-cookie-consent"
+import { DismissAction } from "../alert/dismissaction"
+import { SnackbarKey, snackActions } from "../alert/psalert"
+
+export const VersionNotification = () => {
+  const version = process.env.REACT_APP_VERSION
+  const cookieIsNotSet =
+    Cookies.get(version + "Notify") === undefined ||
+    Cookies.get(version + "Notify") === "false"
+
+  const confirm = useCallback(() => {
+    Cookies.set(version + "Notify", true, { expires: 365 })
+    window.location.href = `#/changelog.html#${process.env.REACT_APP_VERSION}`
+  }, [version])
+
+  const cancel = useCallback(() => {
+    Cookies.set(version + "Notify", true, { expires: 365 })
+  }, [version])
+
+  const action = useCallback((key: SnackbarKey) => {
+    return (
+      <DismissAction
+        key={key}
+        confirmCallback={confirm}
+        cancelCallback={cancel}
+        confirmText={`${version}`}
+      />
+    )
+  }, [])
+
+  useEffect(() => {
+    if (cookieIsNotSet) {
+      snackActions.info("Check out the newest release of ParrotSour!", {
+        style: { pointerEvents: "all" },
+        autoHideDuration: 10000,
+        preventDuplicate: true,
+        action: action,
+      })
+    }
+  }, [])
+
+  return null
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 /* istanbul ignore file */
 import React, { Suspense } from "react"
-import { CookieConsent, Cookies } from "react-cookie-consent"
+import { CookieConsent } from "react-cookie-consent"
 import { createRoot } from "react-dom/client"
-import { DismissAction } from "./components/alert/dismissaction"
 import GlobalSnackbarProvider from "./components/alert/globalalertprovider"
-import { snackActions } from "./components/alert/psalert"
 import { Home } from "./components/home/home"
 import "./css/body.css"
 import "./css/fonts.css"
+import { VersionNotification } from "./components/home/version-notification"
 
 const container = document.getElementById("root")
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -27,38 +26,6 @@ root.render(
       This website uses cookies to enhance the user experience. To learn more,
       please see the release notes.
     </CookieConsent>
+    <VersionNotification />
   </React.StrictMode>
 )
-
-// TODO -- move to a null-returning Component, and put after CookieConsent above?
-export const version = process.env.REACT_APP_VERSION
-const cookieIsNotSet =
-  Cookies.get(version + "Notify") === undefined ||
-  Cookies.get(version + "Notify") === "false"
-
-function confirm() {
-  Cookies.set(version + "Notify", true, { expires: 365 })
-  window.location.href = `#/changelog.html#${process.env.REACT_APP_VERSION}`
-}
-
-function cancel() {
-  Cookies.set(version + "Notify", true, { expires: 365 })
-}
-
-if (cookieIsNotSet) {
-  snackActions.info("Check out the newest release of ParrotSour!", {
-    style: { pointerEvents: "all" },
-    autoHideDuration: 10000,
-    preventDuplicate: true,
-    action: (snackbarkey) => {
-      return (
-        <DismissAction
-          key={snackbarkey}
-          confirmCallback={confirm}
-          cancelCallback={cancel}
-          confirmText={`${version}`}
-        />
-      )
-    },
-  })
-}
