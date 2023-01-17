0664ea792192e74fd3910f752bf335db26ab5d7d
John McCarthy <jemccarthy13@gmail.com>
2023-01-17 15:01:45 +0200

    chore: css clean help icon

--- src/pscomponents/help/help-button.tsx
+++ src/pscomponents/help/help-button.tsx
@@ -4,8 +4,6 @@ import { styled } from "@mui/system"
 export { Button }
 
 export const HelpButton = styled(Button)`
-  color: black;
-  background-color: #eee;
   width: 24px !important;
   height: 24px !important;
   display: inline-block;
@@ -13,11 +11,8 @@ export const HelpButton = styled(Button)`
   font-size: 18px;
   margin-left: 8px !important;
   text-align: center;
-  text-decoration: none;
-  -webkit-box-shadow: inset -1px -1px 1px 0px rgba(0, 0, 0, 0.25);
-  -moz-box-shadow: inset -1px -1px 1px 0px rgba(0, 0, 0, 0.25);
-  box-shadow: inset -1px -1px 1px 0px rgba(0, 0, 0, 0.25);
-  float: right;
   min-width: unset !important;
   align-self: center;
+  padding: 0px;
+  line-height: unset;
 `
