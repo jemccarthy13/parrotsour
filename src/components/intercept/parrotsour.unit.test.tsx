24aaf2b4a38f451fc20be4859d316e26f89103cc
John McCarthy <jemccarthy13@gmail.com>
2023-01-19 17:20:33 +0200

    chore: update snapshot

--- src/components/intercept/parrotsour.unit.test.tsx
+++ src/components/intercept/parrotsour.unit.test.tsx
@@ -2,6 +2,14 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { ParrotSourIntercept } from "./parrotsour"
 
+jest.mock("react-speech-recognition", () => ({
+  useSpeechRecognition: () => ({
+    transcript: "",
+    finalTranscript: "",
+    setTranscript: jest.fn(),
+  }),
+}))
+
 describe("ParrotSourIntercept", () => {
   it("renders", () => {
     const wrapper = render(<ParrotSourIntercept />)
